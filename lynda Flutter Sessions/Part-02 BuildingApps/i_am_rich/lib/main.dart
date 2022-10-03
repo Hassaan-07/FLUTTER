250724db09fc1c59e2c8c2082acff84c67bf355a
HassaanRaheem <hassaanraheem56@gmail.com>
2022-10-04 03:05:05 +0500

    build i'm rich app
    Scaffold , AppBar , NetworkImage ..

--- lynda Flutter Sessions/Part-02 BuildingApps/i_am_rich/lib/main.dart
+++ lynda Flutter Sessions/Part-02 BuildingApps/i_am_rich/lib/main.dart
@@ -7,12 +7,24 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text(
-          'Hello World',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey[700],
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          title: Center(
+            child: Text(
+              'I am rich',
+            ),
+          ),
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3JhWlYmrtwi8Mffjj-f2fD6Q_XdhqmmkWgWXlUwEvPzRt5rG23ZGu0hBxJQ_Q68H8Uaw&usqp=CAU'),
+          ),
         ),
       ),
-      debugShowCheckedModeBanner: false,
     ),
   );
 }
